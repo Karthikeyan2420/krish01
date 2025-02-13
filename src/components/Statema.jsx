8ffe17eca7556522cd9277ee952b213343abd236
karthikeyan2420 <karthikeyan2420@gmail.com>
2025-02-13 17:51:50 +0530

    23

--- src/components/Statema.jsx
+++ src/components/Statema.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 
 export const Statema = () => {
     const [count,setcount]=useState(0)
+    const [name , setname]=useState("")
     const handleadd=()=>
     {
         setcount(count+1)
@@ -11,6 +12,9 @@ export const Statema = () => {
     <>
     <button onClick={handleadd}> +</button>
     <b>{count}</b>
+
+    <div>{name}</div>
+    <input type="text" name="" id="" onChange={(e)=>{setname(e.target.value)}} />
     </>
   )
 }
